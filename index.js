7fef6c4cbccd3fe35f1798dd4be0a670997fd67d
luowei <luv135@163.com>
2023-12-20 00:16:28 +0800

    add gas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ethers } = require("ethers");
-const config = require("./config")
+const config = require("./config");
 
 // 连接到结点
 const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
@@ -8,23 +8,25 @@ const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
 const wallet = new ethers.Wallet(config.privateKey.trim(), provider);
 
 async function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // 转成16进制
-const convertToHexa = (str = '') =>{
-   const res = [];
-   const { length: len } = str;
-   for (let n = 0, l = len; n < l; n ++) {
-      const hex = Number(str.charCodeAt(n)).toString(16);
-      res.push(hex);
-   };
-   return `0x${res.join('')}`;
-}
+const convertToHexa = (str = "") => {
+  const res = [];
+  const { length: len } = str;
+  for (let n = 0, l = len; n < l; n++) {
+    const hex = Number(str.charCodeAt(n)).toString(16);
+    res.push(hex);
+  }
+  return `0x${res.join("")}`;
+};
 
 // 获取当前账户的 nonce
 async function getCurrentNonce(wallet) {
   try {
+    var balance = await wallet.getBalance();
+    console.log("Balance:", ethers.utils.formatEther(balance));
     const nonce = await wallet.getTransactionCount("pending");
     console.log("Nonce:", nonce);
     return nonce;
@@ -53,14 +55,11 @@ async function getGasLimit(hexData, address) {
 
 // 转账交易
 async function sendTransaction(nonce) {
-  let hexData = config.tokenDataHex
-  if (config.tokenJson !== '') {
-    hexData	= convertToHexa(config.tokenJson.trim());
-  }
+  const hexData = convertToHexa(config.tokenJson.trim());
   // 获取实时 gasPrice
   const currentGasPrice = await getGasPrice();
   // 在当前 gasPrice 上增加 一定倍数
-  const gasMultiple = parseInt(String(config.increaseGas * 100))
+  const gasMultiple = parseInt(String(config.increaseGas * 100));
   const increasedGasPrice = currentGasPrice.div(100).mul(gasMultiple);
   // 获取钱包地址
   let address = await wallet.getAddress();
@@ -69,14 +68,22 @@ async function sendTransaction(nonce) {
   }
   // 获取当前 gasLimit 限制
   const gasLimit = await getGasLimit(hexData, address);
+
+  const costFee = ethers.utils.formatEther(increasedGasPrice.mul(gasLimit));
+  console.log(`Gas fee:`, costFee);
+  if (parseFloat(costFee) > 0.01) {
+    console.log(`Gas too many`);
+    return;
+  }
+
   // 付费金额
-  const payPrice = config.payPrice
+  const payPrice = config.payPrice;
 
   console.log("正在打铭文数据的16进制数据", hexData)
 
   const transaction = {
     to: address,
-	// 替换为你要转账的金额
+    // 替换为你要转账的金额
     value: ethers.utils.parseEther(payPrice),
     // 十六进制数据
     data: hexData,
@@ -84,12 +91,12 @@ async function sendTransaction(nonce) {
     nonce: nonce,
     // 设置 gas 价格
     gasPrice: increasedGasPrice,
-	// 限制gasLimit，根据当前网络转账的设置，不知道设置多少的去区块浏览器看别人转账成功的是多少
+    // 限制gasLimit，根据当前网络转账的设置，不知道设置多少的去区块浏览器看别人转账成功的是多少
     gasLimit: gasLimit,
   };
 
   try {
-    const tx = await wallet.sendTransaction(transaction);
+    // const tx = await wallet.sendTransaction(transaction);
     console.log(`Transaction with nonce ${nonce} hash:`, tx.hash);
   } catch (error) {
     console.error(`Error in transaction with nonce ${nonce}:`, error.message);
@@ -99,12 +106,12 @@ async function sendTransaction(nonce) {
 // 发送多次交易
 async function sendTransactions() {
   const currentNonce = await getCurrentNonce(wallet);
-  const sleepTime = config.sleepTime
+  const sleepTime = config.sleepTime;
 
   for (let i = 0; i < config.repeatCount; i++) {
-    //const gasPrice = await getGasPrice();
-    await sendTransaction(currentNonce + i);
-    await sleep(sleepTime)
+    const gasPrice = await getGasPrice();
+    await sendTransaction(currentNonce + i, gasPrice);
+    await sleep(sleepTime);
   }
 }
 
